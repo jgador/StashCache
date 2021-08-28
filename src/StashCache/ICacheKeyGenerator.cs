303174109640d0f06c5acde79e9cc8903ba44cbe
Jesse Gador <jessegador@gmail.com>
2021-08-28 20:59:36 +0800

    CacheKey generator

---
+++ src/StashCache/ICacheKeyGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StashCache
+{
+    public interface ICacheKeyGenerator<TGenerator>
+    {
+        CacheKey GenerateCacheKey<TOwner>([CallerMemberName] string? callerMemberName = null, params string[]? segments);
+    }
+}
---
+++ src/StashCache/TypeCacheKeyGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StashCache
+{
+    public class TypeCacheKeyGenerator : ICacheKeyGenerator<TypeCacheKeyGenerator>
+    {
+        public CacheKey GenerateCacheKey<TOwner>([CallerMemberName] string? callerMemberName = null, params string[]? segments)
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            return new(typeof(TOwner), callerMemberName, segments);
+#pragma warning restore CS8604 // Possible null reference argument.
+        }
+    }
+}
