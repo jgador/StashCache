38d71b4f5b59cacbdae8919a71ec30dea0c333a9
Jesse Gador <jessegador@gmail.com>
2021-08-28 21:55:02 +0800

    StashCacheExtensions

---
+++ src/StashCache/StashCacheExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace StashCache
+{
+    public static class StashCacheExtensions
+    {
+        public static IServiceCollection AddStashCache(this IServiceCollection services)
+        {
+            services.NotNull(nameof(services));
+
+            services.AddMemoryCache();
+
+            services.AddSingleton<ILocalCache, LocalCache>();
+
+            return services;
+        }
+    }
+}
