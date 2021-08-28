f1d4df493ce212055defcb1cf200c25384cbabc5
Jesse Gador <jessegador@gmail.com>
2021-08-28 20:28:52 +0800

    Check.cs class

---
+++ src/StashCache/Check.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StashCache
+{
+    internal static class Check
+    {
+        public static T NotNull<T>(this T value, string paramName) where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+
+            return value;
+        }
+
+        public static T? NotNull<T>(this T? value, string paramName) where T : struct
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+
+            return value;
+        }
+
+        public static string NotEmpty(this string value, string paramName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("The argument cannot be null, empty, or contain only white space.", paramName);
+            }
+
+            return value;
+        }
+    }
+}
