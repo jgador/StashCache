df175ec97396e2ab308ffbd1d7f6085c155cb1dc
Jesse Gador <jessegador@gmail.com>
2021-08-31 20:11:45 +0800

    Added benchmark for specifying number of items in cache

--- src/Benchmarks.StashCache/LocalCacheBenchmarks.cs
+++ src/Benchmarks.StashCache/LocalCacheBenchmarks.cs
@@ -1,11 +1,11 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using StashCache;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -50,6 +50,18 @@ namespace Benchmarks.StashCache
         }
 
         [Benchmark]
+        public async Task LocalCacheGetOrAdd_1k_Async()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            // Add 10k records to cache
+            await AddOrRetrieveFromCacheAsync(1000, cts.Token).ConfigureAwait(false);
+
+            // Retrieve 10k stored records in cache
+            await AddOrRetrieveFromCacheAsync(1000, cts.Token).ConfigureAwait(false);
+        }
+
+        [Benchmark]
         public async Task LocalCacheGetOrAddWithReaderWriterLockSlimAsync()
         {
             CancellationTokenSource cts = new CancellationTokenSource();
@@ -64,24 +76,86 @@ namespace Benchmarks.StashCache
             }, DefaultCacheExpiry, cts.Token).ConfigureAwait(false);
         }
 
+        [Benchmark]
+        public async Task LocalCacheGetOrAdd_1k_WithReaderWriterLockSlimAsync()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            // Add 10k records to cache
+            await AddOrRetrieveFromCacheReaderWriterLockSlimAsync(1000, cts.Token).ConfigureAwait(false);
+
+            // Retrieve 10k stored records in cache
+            await AddOrRetrieveFromCacheReaderWriterLockSlimAsync(1000, cts.Token).ConfigureAwait(false);
+        }
+
+        private async Task AddOrRetrieveFromCacheAsync(int records, CancellationToken cancellationToken)
+        {
+            for (int i = 1; i <= records; i++)
+            {
+                var cacheKey = CacheKeyGenerator.GenerateCacheKey<LocalCacheBenchmarks>(segments: new List<string> { i.ToString() });
+
+                var result = await LocalCache.GetOrAddAsync(cacheKey, async () =>
+                {
+                    var summaries = await GetSummariesAsyc();
+
+                    return summaries;
+
+                }, DefaultCacheExpiry, cancellationToken).ConfigureAwait(false);
+
+                Debug.Assert(result != null);
+            }
+        }
+
+        private async Task AddOrRetrieveFromCacheReaderWriterLockSlimAsync(int records, CancellationToken cancellationToken)
+        {
+            for (int i = 1; i <= records; i++)
+            {
+                var cacheKey = CacheKeyGenerator.GenerateCacheKey<LocalCacheBenchmarks>(segments: new List<string> { i.ToString() });
+
+                var result = await LocalCache.GetOrAddWithReaderWriterLockSlimAsync(cacheKey, async () =>
+                {
+                    var summaries = await GetSummariesAsyc();
+
+                    return summaries;
+
+                }, DefaultCacheExpiry, cancellationToken).ConfigureAwait(false);
+
+                Debug.Assert(result != null);
+            }
+        }
+
         private async Task<IEnumerable<WeatherForecast>> GetSummariesAsyc()
         {
             await Task.CompletedTask;
 
-            var random = new Random();
+            //var random = new Random();
+
+            //return new List<WeatherForecast>()
+            //{
+            //    new WeatherForecast { Summary = "Freezing", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Bracing", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Chilly", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Cool", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Mild", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Warm", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Balmy", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Hot", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Sweltering", TemperatureC = random.Next(-20, 55) },
+            //    new WeatherForecast { Summary = "Scorching", TemperatureC = random.Next(-20, 55) }
+            //}.ToArray();
 
             return new List<WeatherForecast>()
             {
-                new WeatherForecast { Summary = "Freezing", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Bracing", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Chilly", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Cool", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Mild", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Warm", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Balmy", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Hot", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Sweltering", TemperatureC = random.Next(-20, 55) },
-                new WeatherForecast { Summary = "Scorching", TemperatureC = random.Next(-20, 55) }
+                new WeatherForecast { Summary = "Freezing", TemperatureC = 1 },
+                new WeatherForecast { Summary = "Bracing", TemperatureC = 2 },
+                new WeatherForecast { Summary = "Chilly", TemperatureC = 3 },
+                new WeatherForecast { Summary = "Cool", TemperatureC = 4 },
+                new WeatherForecast { Summary = "Mild", TemperatureC = 5 },
+                new WeatherForecast { Summary = "Warm", TemperatureC = 6 },
+                new WeatherForecast { Summary = "Balmy", TemperatureC = 7 },
+                new WeatherForecast { Summary = "Hot", TemperatureC = 8 },
+                new WeatherForecast { Summary = "Sweltering", TemperatureC = 9 },
+                new WeatherForecast { Summary = "Scorching", TemperatureC = 10 }
             }.ToArray();
         }
 
