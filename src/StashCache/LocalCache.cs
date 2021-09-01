e4099e215492de359a05469d09641054f71a7439
Jesse Gador <jessegador@gmail.com>
2021-09-02 03:33:03 +0800

    Removed the concurrent dictionary overhead for GetOrAddAsync of cache item

--- src/StashCache/LocalCache.cs
+++ src/StashCache/LocalCache.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace StashCache
 {
-    public sealed class LocalCache : ILocalCache
+	public sealed class LocalCache : ILocalCache
     {
-        private static readonly ConcurrentDictionary<CacheKey, object> _locks = new ConcurrentDictionary<CacheKey, object>();
         private ReaderWriterLockSlim _readerWriterLock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
         private readonly IMemoryCache _memoryCache;
         private readonly ILogger<LocalCache> _logger;
@@ -20,56 +18,18 @@ namespace StashCache
             _logger = logger.NotNull(nameof(logger));
         }
 
-        public async Task<TResult> GetOrAddAsync<TResult>(CacheKey cacheKey, Func<Task<TResult>> valueFactory, TimeSpan timeToLive, CancellationToken cancellationToken)
+        public async Task<TItem> GetOrAddAsync<TItem>(CacheKey cacheKey, Func<Task<TItem>> valueFactory, TimeSpan timeToLive, CancellationToken cancellationToken)
         {
             valueFactory.NotNull(nameof(valueFactory));
 
-            if (_memoryCache.TryGetValue(cacheKey, out TResult cacheItem))
+            if (!_memoryCache.TryGetValue(cacheKey, out object result))
             {
-                _logger.LogDebug($"Cache hit: {cacheKey}");
+                result = await valueFactory().ConfigureAwait(false);
 
-                return cacheItem;
+                _logger.LogDebug($"Cache miss: {cacheKey}");
             }
 
-            cacheItem = await valueFactory().ConfigureAwait(false);
-
-            if (cacheItem == null)
-            {
-                throw new NotSupportedException("Does not support caching of null value.");
-            }
-
-            _logger.LogDebug($"Cache miss: {cacheKey}");
-
-            _memoryCache.Set(cacheKey, cacheItem, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = timeToLive });
-
-            return cacheItem;
-
-            //var newCacheValue = _locks.GetOrAdd(cacheKey, cachedValue);
-
-            //try
-            //{
-            //    lock (newCacheValue)
-            //    {
-            //        if (_memoryCache.TryGetValue(cacheKey, out cachedValue))
-            //        {
-            //            _logger.LogDebug($"Cache hit: {cacheKey}");
-            //        }
-            //        else
-            //        {
-            //            _logger.LogDebug($"Cache miss: {cacheKey}");
-
-            //            cachedValue = (TResult)newCacheValue;
-
-            //            _memoryCache.Set(cacheKey, cachedValue, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = timeToLive });
-            //        }
-
-            //        return cachedValue;
-            //    }
-            //}
-            //finally
-            //{
-            //    _locks.TryRemove(cacheKey, out _);
-            //}
+            return (TItem)result;
         }
 
         public async Task<TResult> GetOrAddWithReaderWriterLockSlimAsync<TResult>(CacheKey cacheKey, Func<Task<TResult>> valueFactory, TimeSpan timeToLive, CancellationToken cancellationToken)
