33285dced0adefc25647f077da3ad09863fb05fe
Jesse Gador <jessegador@gmail.com>
2021-09-04 23:58:20 +0800

    Added benchmark in README

--- src/StashCache/StashCacheExtensions.cs
+++ src/StashCache/StashCacheExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace StashCache
 {
+    /// <summary>
+    /// Extension methods for setting up local in-memory services in an Microsoft.Extensions.DependencyInjection.IServiceCollection.
+    /// </summary>
     public static class StashCacheExtensions
     {
+        /// <summary>
+        /// Adds services for local in-memory cache to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add services to.</param>
+        /// <returns>An Microsoft.Extensions.DependencyInjection.IServiceCollection.</returns>
         public static IServiceCollection AddStashCache(this IServiceCollection services)
         {
             services.NotNull(nameof(services));
