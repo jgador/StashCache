799950c0bd6e90db87297247eacd3c569452c70b
Jesse Gador <jessegador@gmail.com>
2021-09-04 21:28:52 +0800

    Make CacheKey case-sensitive

--- src/Sample.AspNetCore/Services/WeatherForecastService.cs
+++ src/Sample.AspNetCore/Services/WeatherForecastService.cs
@@ -24,7 +24,6 @@ namespace Sample.AspNetCore.Services
             var hashCode = cacheKey.GetHashCode();
 
             var result = await _localCache.GetOrCreateAsync(cacheKey, async () =>
-            // var result = await _localCache.GetOrAddWithReaderWriterLockSlimAsync(cacheKey, async () =>
             {
                 var summaries = await GetSummariesAsyc();
 
@@ -37,11 +36,10 @@ namespace Sample.AspNetCore.Services
 
         public async Task<IEnumerable<WeatherForecast>> GetBySummaryAsync(string summary, CancellationToken cancellationToken)
         {
-            var cacheKey = CacheKeyGenerator.GenerateCacheKey<WeatherForecastService>(segments: new string[] { summary, null });
+            var cacheKey = CacheKeyGenerator.GenerateCacheKey<WeatherForecastService>(segments: new string[] { summary.ToLower(), null });
             var hashCode = cacheKey.GetHashCode();
 
             var cachedValues = await _localCache.GetOrCreateAsync(cacheKey, async () =>
-            // var cachedValues = await _localCache.GetOrAddWithReaderWriterLockSlimAsync(cacheKey, async () =>
             {
                 var result = (await GetSummariesAsyc())
                     .Where(a => a.Summary.ToLower() == summary.ToLower());
@@ -49,7 +47,6 @@ namespace Sample.AspNetCore.Services
                 return result;
 
             }, DefaultCacheExpiry, cancellationToken).ConfigureAwait(false);
-
 
             return cachedValues;
         }
--- src/StashCache/CacheKey.cs
+++ src/StashCache/CacheKey.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace StashCache
 {
     /// <summary>
-    /// A key that uniquely identifies a local in-memory cache value.
+    /// A case-sensitive key that uniquely identifies a local in-memory cache value.
     /// </summary>
     [DebuggerDisplay("{ToString()}")]
     public readonly struct CacheKey : IEquatable<CacheKey>
@@ -51,9 +51,9 @@ namespace StashCache
 
         public bool Equals(CacheKey other)
         {
-            return _ownerType.FullName == other._ownerType.FullName
+            return _ownerType.Equals(other._ownerType)
                 && _memberInfo == other._memberInfo
-                && string.Compare(_segmentString, other._segmentString, StringComparison.OrdinalIgnoreCase) == 0;
+                && _segmentString == other._segmentString;
         }
 
         public override int GetHashCode()
@@ -61,7 +61,7 @@ namespace StashCache
             var hash = new HashCode();
             hash.Add(_ownerType.FullName);
             hash.Add(_memberInfo);
-            hash.Add(_segmentString?.ToLower());
+            hash.Add(_segmentString);
 
             return hash.ToHashCode();
         }
